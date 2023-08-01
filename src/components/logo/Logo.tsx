3e488634807fda64bfbf95dbd5af7be92677530d
PatrykRoskosz <roskoszpatryk55@gmail.com>
2023-08-01 07:01:10 +0200

    final

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,10 +1,11 @@
 import "./Logo.scss";
-import { FaFacebook ,FaFacebookF} from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
 	return (
 		<div className='logo'>
-			<h1 className="header-logo ">TomTom - Transfer</h1>
+		<Link to="/"><h1 className="header-logo ">TomTom - Transfer</h1></Link>	
 			<a
 				href='https://www.facebook.com/people/Tomtom-transfer/100076940167167/?paipv=0&eav=AfbfEAUc1SuNtotugEUunyz2--M9nH8lDqGe8hNZa79VovO61W9amYLVpEAZp06PsN0&_rdr'
 				target='blanc'
